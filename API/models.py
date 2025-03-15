7eb63ad43dd90ab1c468d326f5346fbd5977fb0a
pratyusha2021 <doyel1200@gmail.com>
2025-03-15 23:19:11 +0530

    Added serializers

--- API/models.py
+++ API/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length = 20)
+    category = models.CharField(max_length = 20)
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    barcode = models.IntegerField(unique = True)
